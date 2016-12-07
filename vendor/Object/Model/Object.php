f320e7173a135c545d0d1d2cc291e91a94d9b889
Tom Groskamp <Tom.Groskamp@Itonomy.nl>
2016-12-07 23:03:10 +0100

    added magic methods to base Object

--- vendor/Object/Model/Object.php
+++ vendor/Object/Model/Object.php
@@ -11,6 +11,21 @@ class Object
     private $objectManager;
 
     /**
+     * @var array
+     */
+    private $data = array();
+
+    /**
+     * @var array
+     */
+    private $magicMethods = array(
+        'get',
+        'uns',
+        'has',
+        'set',
+    );
+
+    /**
      * Object constructor.
      */
     public function __construct()
@@ -18,9 +33,136 @@ class Object
         $this->objectManager = new ObjectManager;
     }
 
-    public function __get($get)
+    /**
+     * Call magic function
+     * Used for getting, setting, unsetting, hassing.
+     *
+     * @param $name
+     * @param $arguments
+     * @return mixed
+     * @throws \Exception
+     */
+    public function __call($name, $arguments)
     {
-        echo 'we get here, gettit?';
+        /**
+         * Extract the magic method call and the index used for the magic method.
+         */
+        $functionName = substr($name, 0, 3);
+        $index = substr($name, 3);
+
+        /**
+         * Put the index at the beginning of the argument list,
+         * and call the requested magic method for the given arguments.
+         */
+        array_unshift($arguments, lcfirst($index));
+        if (!in_array($functionName, $this->magicMethods)) {
+            throw new \Exception(
+                \Object\Declarations::ERROR_MAGIC_METHOD_UNDEFINED,
+                \Object\Declarations::ERROR_MAGIC_METHOD_UNDEFINED_CODE
+            );
+        }
+
+        /**
+         * Return the result of the magic function.
+         */
+        return call_user_func_array(array($this, $functionName), $arguments);
+    }
+
+    /**
+     * Returns a value magically, or false if the requested value is not set.
+     * When strict is set to true, will throw an Exception if the value is not set.
+     *
+     * @param $index
+     * @param bool $strict
+     * @return mixed
+     * @throws \Exception
+     */
+    public function get($index, $strict = false)
+    {
+        /**
+         * If strict parameter is provided, validate if the index is present in $data.
+         */
+        if ($strict == true && !$this->has($index)) {
+            $this->validate($index);
+        }
+
+        /**
+         * Return the set value, or false if not set.
+         */
+        return $this->has($index) ? $this->data[$index] : false;
+    }
+
+    /**
+     * Sets the value of an index in $data.
+     * When strict is set to true, will throw an Exception if the value is not yet set already.
+     *
+     * @param $index
+     * @param null $value
+     * @param bool $strict
+     * @return $this
+     * @throws \Exception
+     */
+    public function set($index, $value = null, $strict = false)
+    {
+        /**
+         * If strict parameter is provided, validate if the index is present in $data.
+         */
+        if ($strict == true && !$this->has($index)) {
+            $this->validate($index);
+        }
+
+        $this->data[$index] = $value;
+        return $this;
+    }
+
+    /**
+     * Resets the value of an index in $data to null.
+     * When strict is set to true, will throw an Exception if the value is not set.
+     *
+     * @param $index
+     * @param bool $strict
+     * @return $this
+     * @throws \Exception
+     */
+    public function uns($index, $strict = false)
+    {
+        /**
+         * If strict parameter is provided, validate if the index is present in $data.
+         */
+        if ($strict == true && !$this->has($index)) {
+            $this->validate($index);
+        }
+
+        $this->data[$index] = null;
+        return $this;
+    }
+
+    /**
+     * True if set, false if not.
+     *
+     * @param $index
+     * @return bool
+     */
+    public function has($index)
+    {
+        return isset($this->data[$index]);
+    }
+
+    /**
+     * Throw an Exception if index is not set in $data.
+     *
+     * @param $index
+     * @throws \Exception
+     */
+    public function validate($index)
+    {
+        $message = sprintf(\Object\Declarations::ERROR_STRICT_MAGIC_FUNCTION, $index) . PHP_EOL .
+            implode(' -=- ', array_keys($this->data));
+
+        throw new \Exception(
+            $message,
+            \Object\Declarations::ERROR_STRICT_MAGIC_FUNCTION_CALL_CODE
+        );
     }
 
     /**
