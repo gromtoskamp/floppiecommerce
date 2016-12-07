6656dc94beac0e5264007badee1f0cb4151319a0
Tom Groskamp <Tom.Groskamp@Itonomy.nl>
2016-12-07 23:09:12 +0100

    cleaned up base request model

--- vendor/Router/Model/Request.php
+++ vendor/Router/Model/Request.php
@@ -6,15 +6,11 @@ use Object\Model\Object;
 
 class Request extends Object
 {
+    /**
+     * Request constructor.
+     */
     public function __construct()
     {
-        print_r($this->getId());
-        $this->setId('testId2');
-        print_r($this->getId());
-        $this->unsId();
-        var_dump($this->getId());
-        print_r($this->unsId(true));
-
-        exit;
+        parent::__construct();
     }
 }
