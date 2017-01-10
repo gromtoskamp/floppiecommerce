352d46bba26b2b6026e387ab5c6918e3c25855b4
Tom Groskamp <Tom.Groskamp@Itonomy.nl>
2017-01-10 09:13:45 +0100

    added beginning of scanning for commands

--- vendor/Cli/Controller/Cli.php
+++ vendor/Cli/Controller/Cli.php
@@ -4,6 +4,7 @@ namespace Cli\Controller;
 
 use Object\Model\Object;
 use Cli\Model\Cli as CliModel;
+use Cli\Command\Hub;
 
 class Cli extends Object
 {
@@ -13,17 +14,25 @@ class Cli extends Object
     protected $cli;
 
     /**
+     * @var \Cli\Command\Hub
+     */
+    protected $hub;
+
+    /**
      * Cli constructor.
      */
-    public function __construct(
-    ) {
+    public function __construct() {
         parent::__construct();
         $this->cli = $this->getInstance(CliModel::class);
+        $this->hub = $this->getInstance(Hub::class);
     }
 
     public function run()
     {
-        
+        $this->cli->writeLine('Welcome!');
+
+        //TODO: make this work.
+        $this->hub->scan();
     }
 
 }
