f0171adae3f46737c0db642aeaaa75a1e9596b8a
Tom Groskamp <Tom.Groskamp@Itonomy.nl>
2017-01-14 21:04:16 +0100

    Introducing: Addons! Independent external extensions which are included at runtime. They said it couldnt be done, and it probably shouldnt have been done. - vendor/Object/Model/Di.php Added: setAddon 	Adds an addon reference for a given class. Added: validateAddon 	Checks for the base class, the addon class, and the previously set addons for the base class if any of their functions have the same name. Added: getAddons 	Returns all addons for a given class, or an empty array if none are found. Moved: rewrite Moved: getRewrite Moved: parseClassname 	Moved from ObjectManager to Di. Also made these function calls static. - vendor/Object/Model/Object.php Modified: __call 	Added Addon function checking. Added: checkAddonsForFunctions 	Checks if any of the addons has the required function defined. If the function is found, just call it right away. Added: addAddons 	Adds the addons as an indexed list of properties. The addon magic will know how to call the functions. Added: getAddonClasses 	Gets all Addons for a given class.

--- vendor/Object/Model/ObjectManager.php
+++ vendor/Object/Model/ObjectManager.php
@@ -4,22 +4,6 @@ namespace Object\Model;
 
 class ObjectManager
 {
-
-    /**
-     * 100
-     * Class not found.
-     */
-    const ERROR_CLASS_NOT_FOUND_CODE = '100';
-    const ERROR_CLASS_NOT_FOUND = 'Class %s does not exist!';
-
-    /**
-     * 101
-     * Double rewrite.
-     */
-    const ERROR_DOUBLE_REWRITE_CODE = '101';
-    const ERROR_DOUBLE_REWRITE = 'Double rewrite found for object %s! Rerwrite requested: %s, Rewrite found: %s.';
-
-
     /**
      * Array of singletons in the from of:
      *  array(
@@ -29,8 +13,6 @@ class ObjectManager
      * @var array
      */
     private $singletons = array();
-
-    private static $rewrites = array();
 
     /**
      * Gets the class defined in $class, or the rewrite of this $class.
@@ -92,78 +74,6 @@ class ObjectManager
      */
     public function parseClassname($class)
     {
-        /**
-         * Namespaces should be defined with a starting '\'.
-         *
-         * If this is not provided,
-         * add it to the namespace for convenience and singleton persistence.
-         */
-        if (substr($class, 0, 1) !== '\\') {
-            $class = '\\' . $class;
-        }
-
-        /**
-         * Check if a rewrite is defined and return the rewritten namespace if found.
-         */
-        $class = $this->getRewrite($class) ? $this->getRewrite($class) : $class;
-
-        /**
-         * If the parsed class does not exist, throw an exception to inform the user of this mishap.
-         */
-        if (!class_exists($class)) {
-            throw new \Exception(
-                sprintf(self::ERROR_CLASS_NOT_FOUND, $class),
-                self::ERROR_CLASS_NOT_FOUND_CODE
-            );
-        }
-
-        return $class;
+        return Di::parseClassname($class);
     }
-
-    /**
-     * Sets a rewrite in the static $rewrites array.
-     * This method is static, to make sure that rewrites can be added from init.php files.
-     *
-     * @param string $from (\Namespace\Path\To\Original)
-     * @param string $to (\Namespace\Path\To\Rewrite)
-     * @throws \Exception in case of a double rewrite.
-     */
-    public static function setRewrite($from, $to)
-    {
-        /**
-         * If a conflicting rewrite is found, throw an exception informing the user.
-         */
-        if (isset(self::$rewrites[$from])) {
-            throw new \Exception(
-                sprintf(
-                    self::ERROR_DOUBLE_REWRITE,
-                    $from,
-                    $to,
-                    self::$rewrites[$from]),
-                self::ERROR_DOUBLE_REWRITE_CODE
-            );
-        }
-
-        /**
-         * Return the namespace of the rewrite.
-         */
-        self::$rewrites[$from] = $to;
-    }
-
-    /**
-     * Returns a rewrite if this is found,
-     * otherwise returns false for comparison purposes.
-     *
-     * @param $from
-     * @return string | bool
-     */
-    public function getRewrite($from)
-    {
-        if (isset(self::$rewrites[$from])) {
-            return self::$rewrites[$from];
-        }
-
-        return false;
-    }
-
 }
